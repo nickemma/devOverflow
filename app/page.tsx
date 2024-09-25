57ec11fb22d403c5278c135529ef948615d11518
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2024-09-26 00:55:23 +0100

    Added tailwindcss and dark and light mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
-    <div className="bg-gray-500">
-      <h2>Welcome to next JS world</h2>
+    <div>
+      <h1 className="h1-bold">Welcome to next JS world</h1>
     </div>
   );
 };
