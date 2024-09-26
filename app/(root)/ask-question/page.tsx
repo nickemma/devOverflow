f8edb8c461fadda51add5bb7c5fca09a1b5951dd
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2024-09-27 00:34:29 +0100

    Added clerk for authentication and set up the signIn and signUp page

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AskQuestion = () => {
+  return <div>Ask a questions</div>;
+};
+
+export default AskQuestion;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <div>
-      <h1 className="h1-bold">Welcome to next JS world</h1>
-    </div>
-  );
-};
-
-export default Home;
